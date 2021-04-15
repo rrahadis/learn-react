a18969a89d1069fc9a14cacbc6d5ab8898fea1f3
Rahadi <43909807+rrahadis@users.noreply.github.com>
2021-04-15 16:36:40 +0700

    structured code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
     return (
-        <div className = "expense-item" > 
+        <Card className = "expense-item" > 
         <ExpenseDate date={props.date}></ExpenseDate>
             <div className = "expense-item__description" >
                 <h2>{props.title}</h2>
@@ -12,7 +13,7 @@ const ExpenseItem = (props) => {
             <div className = "expense-item__price" >
                 ${props.amount}
             </div>
-        </div>
+        </Card>
     )
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from '../UI/Card';
 
 const Expenses = (props) => {
   return (
-    <div className = "expenses">
+    <Card className = "expenses">
       <ExpenseItem
         title={props.detail[0].title}
         amount={props.detail[0].amount}
@@ -20,7 +21,7 @@ const Expenses = (props) => {
         amount={props.detail[2].amount}
         date={props.detail[2].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 };
 
